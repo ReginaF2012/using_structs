8301ca2fc618c6567e27b44ed013c48637513b16
ReginaF2012 <skritschy@gmail.com>
2020-12-04 13:23:38 -0600

    using method syntax

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,44 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
-    
-    println!("rect1 is {:?}", rect1);
+
+    let rect2 = Rectangle {
+        width: 20,
+        height: 30,
+    };
+
+    let rect3 = Rectangle {
+        width: 40,
+        height: 44,
+    };
 
     println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        "The area of the first rectangle is {} square pixels.",
+        rect1.area()
     );
-}
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!(
+        "Rectangle 1 can hold rectangle 2: {}", rect1.can_hold(&rect2)
+    );
+
+    println!(
+        "Rectangle 1 can hold rectangle 3: {}", rect1.can_hold(&rect3)
+    );
 }
